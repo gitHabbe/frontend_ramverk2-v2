74a6969d8248df46a6a975085cc353cc2f10111c
Niklas Hallberg <91habbe@gmail.com>
2019-01-29 16:05:18 +0100

    added onChange and onSubmit for NewReport form

--- src/Components/NewReport.js
+++ src/Components/NewReport.js
@@ -1,6 +1,23 @@
 import React, { Component } from 'react';
 
 class NewReport extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            kmomNum: "",
+            report: ""
+        }
+    }
+    
+    onChange = e => this.setState({ [e.target.name]: e.target.value });
+
+    onSubmit(e) {
+        e.preventDefault();
+        console.log(this.state.kmomNum);
+        
+        console.log("Submitted");
+    }
+
     render() {
         return (
             <div className="container">
@@ -9,17 +26,21 @@ class NewReport extends Component {
                         <div className="field">
                             <label className="label">Kmom number: </label>
                             <div className="control">
-                                <input className="input" type="text" placeholder="Text input" />
+                                <input onChange={this.onChange.bind(this)} className="input" name="kmomNum" type="text" placeholder="Kmom #" />
                             </div>
                         </div>
                         <div className="field">
                             <label className="label">Report</label>
                             <div className="control">
-                                <textarea className="textarea" placeholder="Textarea"></textarea>
+                                <textarea onChange={this.onChange.bind(this)} name="report" className="textarea" placeholder="Report text"></textarea>
                             </div>
                         </div>
                         <div class="control column is-centered">
-                            <button class="button is-centered is-link">Submit report</button>
+                            <button
+                                onClick={this.onSubmit.bind(this)}
+                                class="button is-centered is-link">
+                                Submit report
+                            </button>
                         </div>
                     </form>
                 </div>
